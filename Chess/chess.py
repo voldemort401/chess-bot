6d7853c0f7b9814369ee1959f79f71f121a38b2d
voldemort401 <biblecampvictim6900@gmail.com>
2025-04-04 20:39:02 +0545

    more fixes yay i hate my life

--- Chess/chess.py
+++ Chess/chess.py
@@ -5,37 +5,71 @@ from pieces.queen import queen
 from pieces.pawn import pawn
 from pieces.rook import rook
 from pieces.knight import knight
-def generatePseudoLegalMoves(board):
+def generatePseudoLegalMoves(board, piece:str=None):
     all_pseudo_moves    = [] 
-    turn                = board[-1]
-    for i,j in enumerate(board):
-        print(i)
-        if (j != EMPTY):
-            if (turn == BLACK):
-                if (j == BKNIGHT):
-                    all_pseudo_moves.append(f'black-knight on {board_sqs[i]} : {knight(board_sqs[i], board)}')
-                if (j == BKING):               
-                    all_pseudo_moves.append(f'black-king on {board_sqs[i]} : {king(board_sqs[i], board)}')
-                if (j == BROOK):
-                    all_pseudo_moves.append(f'black-rook on {board_sqs[i]} : {rook(board_sqs[i], board)}')
-                if (j == BBISHOP): 
-                    all_pseudo_moves.append(f'black-bishop on {board_sqs[i]} : {bishop(board_sqs[i], board)}')
-                if (j == BPAWN): 
-                    all_pseudo_moves.append(f'black-pawn on {board_sqs[i]} : {pawn(board_sqs[i], board)}')
-                if (j == BQUEEN):        
-                    all_pseudo_moves.append(f'black-queen on {board_sqs[i]} : {queen(board_sqs[i], board)}')
-            else:
-                if (j == WKNIGHT):                     
-                    all_pseudo_moves.append(f'white-knight on {board_sqs[i]}: {knight(board_sqs[i], board)}')
-                if (j == WKING):              
-                    all_pseudo_moves.append(f'white-king on {board_sqs[i]} : {king(board_sqs[i], board)} ')
-                if (j == WROOK):
-                    all_pseudo_moves.append(f'white-rook on {board_sqs[i]} : {rook(board_sqs[i], board)} ')
-                if (j == WBISHOP): 
-                    all_pseudo_moves.append(f'white-bishop on {board_sqs[i]} {bishop(board_sqs[i], board)} ')
-                if (j == WPAWN): 
-                    all_pseudo_moves.append(f'white-pawn on {board_sqs[i]} : {pawn(board_sqs[i], board)} ')
-                if (j == WQUEEN):       
-                    all_pseudo_moves.append(f'white-queen on {board_sqs[i]} : {queen(board_sqs[i], board)} ')
-                    
+    turn                = board[64]
+    if (piece != None):
+        for i,j in enumerate(board):
+         print(i)
+         if (j != EMPTY):
+             if (turn == BLACK):
+                 if (j == BKNIGHT and piece == 'N' or piece == 'n'):
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {knight(board_sqs[i], board)}')
+                 elif (j == BKING and piece == 'K' or piece == 'k'):               
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {king(board_sqs[i], board)}')
+                 elif (j == BROOK and piece == 'R' or piece == 'r'):
+                    all_pseudo_moves.append(f'{board_sqs[i]} : {rook(board_sqs[i], board)}')
+                 elif (j == BBISHOP and piece == 'B' or piece == 'b'): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {bishop(board_sqs[i], board)}')
+                 elif (j == BPAWN and piece == ''): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {pawn(board_sqs[i], board)}')
+                 elif (j == BQUEEN and piece == 'Q' or piece == 'q'):        
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {queen(board_sqs[i], board)}')
+             else:
+                 if (j == WKNIGHT and piece == 'N' or piece == 'n'):                     
+                     all_pseudo_moves.append(f'{board_sqs[i]}: {knight(board_sqs[i], board)}')
+                 elif (j == WKING and piece == 'K' or piece == 'k'):              
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {king(board_sqs[i], board)} ')
+                 elif (j == WROOK and piece == 'R' or piece == 'r'):
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {rook(board_sqs[i], board)} ')
+                 elif (j == WBISHOP and piece == 'B' or piece == 'b'): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} {bishop(board_sqs[i], board)} ')
+                 elif (j == WPAWN and piece == ''): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {pawn(board_sqs[i], board)} ')
+                 elif (j == WQUEEN and piece == 'Q' or piece == 'q'):       
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {queen(board_sqs[i], board)} ')
+    
+     
+    else:
+        for i,j in enumerate(board):
+         print(i)
+         if (j != EMPTY):
+             if (turn == BLACK):
+                 if (j == BKNIGHT):
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {knight(board_sqs[i], board)}')
+                 if (j == BKING):               
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {king(board_sqs[i], board)}')
+                 if (j == BROOK):
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {rook(board_sqs[i], board)}')
+                 if (j == BBISHOP): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {bishop(board_sqs[i], board)}')
+                 if (j == BPAWN): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {pawn(board_sqs[i], board)}')
+                 if (j == BQUEEN):        
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {queen(board_sqs[i], board)}')
+             else:
+                 if (j == WKNIGHT):                     
+                     all_pseudo_moves.append(f'{board_sqs[i]}: {knight(board_sqs[i], board)}')
+                 if (j == WKING):              
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {king(board_sqs[i], board)} ')
+                 if (j == WROOK):
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {rook(board_sqs[i], board)} ')
+                 if (j == WBISHOP): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} {bishop(board_sqs[i], board)} ')
+                 if (j == WPAWN): 
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {pawn(board_sqs[i], board)} ')
+                 if (j == WQUEEN):       
+                     all_pseudo_moves.append(f'{board_sqs[i]} : {queen(board_sqs[i], board)} ')
+                     
     return all_pseudo_moves
+    
--- pieces/pawn.py
+++ pieces/pawn.py
@@ -1,4 +1,3 @@
-## broken
 from Chess.vars import *
 def pawn(piece_pos:str, board:list[str]):
     piece_index = board_sqs.index(piece_pos)
@@ -7,13 +6,11 @@ def pawn(piece_pos:str, board:list[str]):
 
     plegal_sq = []
     canMove2squares = False 
-    if (color == WHITE or rank == '2'):
+    if (color == WHITE and rank == '2' and board[piece_index-16] == EMPTY and board[piece_index-8] == EMPTY):
         canMove2squares =  True 
-    print(color, rank)
-    if (color == BLACK and rank == '7'):
+    if (color == BLACK and rank == '7' and board[piece_index+16] == EMPTY and board[piece_index+8] == EMPTY):
         canMove2squares = True
-
-
+    
     for i in range(2):  ## because the maximum moves the pawn can do in one turn is 2 
         if (canMove2squares):
             if (color == WHITE):
@@ -22,5 +19,33 @@ def pawn(piece_pos:str, board:list[str]):
             elif (color == BLACK):
                 piece_index+=8 
                 plegal_sq.append(piece_index)
-    ## ENPASSANT
+        else:
+            if (color == WHITE and board[piece_index-8] == EMPTY):
+                plegal_sq.append(piece_index-8)
+            elif (color == BLACK and board[piece_index+8] == EMPTY):
+                plegal_sq.append(piece_index+8)
+
+    ## currently if the piece is in the edges some funky stuff happens FIXIT LATER 
+    if (color == WHITE and board[piece_index-9][:3] != color and board[piece_index-9] != EMPTY):
+        plegal_sq.append(piece_index-9)          
+    if (color == WHITE and board[piece_index-7][:3] != color and board[piece_index-7] != EMPTY):
+        plegal_sq.append(piece_index-7)
+
+    if (color == BLACK and board[piece_index+9][:3] != color and board[piece_index+9] != EMPTY):
+        plegal_sq.append(piece_index+9)
+    if (color == BLACK and board[piece_index+7][:3] != color and board[piece_index+7] != EMPTY):
+        plegal_sq.append(piece_index+7)
+    
+    ## enpassant
+    possible_enpassant_squares = []
+    piece_index                = board_sqs.index(piece_pos) 
+    if (color == WHITE):
+        possible_enpassant_squares.append(piece_index-9)
+        possible_enpassant_squares.append(piece_index-7)
+    elif (color == BLACK):
+        possible_enpassant_squares.append(piece_index+9)
+        possible_enpassant_squares.append(piece_index+7)
+
+    
+
     return plegal_sq
