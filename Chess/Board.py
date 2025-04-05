ecc9dfe1110a46ce1a9b951ce1ff5538fb095dc3
voldemort401 <biblecampvictim6900@gmail.com>
2025-04-05 20:00:34 +0545

    adding move function in the board

--- Chess/Board.py
+++ Chess/Board.py
@@ -60,18 +60,73 @@ class board():
     
     def Move(self, move: str):
         board = self.__get_current_board__()
-        print(self.fen)
-        ## N x d 4 + 
-        if (len(move) > 5):
+        turn = self.board[64]
+        if (turn == WHITE):
+            move_counter=self.board[-1]
+            move_counter+=1
+            self.board.insert(65, move_counter)
+    
+        Piece_is_pawn = False
+        if (move.rfind('x')  != -1):
+            isCapturing = True
+            move = move.replace('x','')
+
+        if (move.rfind('+')  != -1):
+            isChecking = True    
+            move = move.replace('+','')
+
+        if (move[0] in ('a','b','c','d','e','f','g','h')):
+            Piece_is_pawn=True 
+
+        if (len(move) > 3):
             return 'ERR: Incorrect notation'
+
+        piece = move[0] if Piece_is_pawn == False else ''
+        target_square = move[1:]
 
         Plegal_moves  = generatePseudoLegalMoves(board, piece) 
 
+        if (turn == WHITE):
+            if (piece == 'N' or piece == 'n'):                    
+                piece = WKNIGHT                                                                                          
+            elif (piece == 'K' or piece == 'k'):              
+                piece = WKING
+            elif (piece == 'R' or piece == 'r'):
+                piece = WROOK
+            elif (piece == 'B' or piece == 'b'): 
+                piece = WBISHOP
+            elif (piece == ''): 
+                piece = WPAWN
+            elif (piece == 'Q' or piece == 'q'):       
+                piece = WQUEEN
+        else:
+            if (j  == 'N' or piece == 'n'):
+                piece = BKNIGHT
+            elif ( piece == 'K' or piece == 'k'):               
+                piece = BKING
+            elif ( piece == 'R' or piece == 'r'):
+                piece = BROOK
+            elif ( piece == 'B' or piece == 'b'): 
+                piece = BBISHOP
+            elif ( piece == ''): 
+                piece = BPAWN
+            elif ( piece == 'Q' or piece == 'q'):        
+                piece = BQUEEN
+                                                                                          
+
         for i,j in enumerate(Plegal_moves):
-            if (target_square in j):
-                print('hallop')
+            if (j.__contains__(str(board_sqs.index(target_square)))):    
+              j = j.split(':') # splitting into 2 part eg: ['a2', '[moves]'] makes it easier to only get the moves portion
+              old_piece_pos = board_sqs.index(j[0].lstrip())
+              new_piece_pos = board_sqs.index(target_square)
+              self.board.pop(old_piece_pos)
+              self.board.insert(old_piece_pos, EMPTY)
+              self.board.insert(new_piece_pos, piece)
+              break
 
+        return self.board
 
+    
 
     def __call__(self):
         return self.__create_board__()
