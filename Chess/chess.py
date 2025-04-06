a341ea41ab49a922c2e4bdd9350994392e67a384
voldemort401 <biblecampvictim6900@gmail.com>
2025-04-06 08:34:25 +0545

    added the ischeck() function

--- Chess/chess.py
+++ Chess/chess.py
@@ -5,43 +5,43 @@ from pieces.queen import queen
 from pieces.pawn import pawn
 from pieces.rook import rook
 from pieces.knight import knight
-def generatePseudoLegalMoves(board, piece:str=None):
+def generatePseudoLegalMoves(board, piece:str=None, color=None):
     all_pseudo_moves    = [] 
     turn                = board[64]
     if (piece != None):
         for i,j in enumerate(board):
          if (j != EMPTY):
-             if (turn == BLACK):
-                 if (j == BKNIGHT and piece == 'N' or piece == 'n'):
+             if (turn == BLACK or color == BLACK):
+                 if (j == BKNIGHT and piece == 'N'):
                      all_pseudo_moves.append(f'{board_sqs[i]}: {knight(board_sqs[i], board)}')
-                 elif (j == BKING and piece == 'K' or piece == 'k'):               
+                 elif (j == BKING and piece == 'K'):               
                      all_pseudo_moves.append(f'{board_sqs[i]}: {king(board_sqs[i], board)}')
-                 elif (j == BROOK and piece == 'R' or piece == 'r'):
+                 elif (j == BROOK and piece == 'R'):  
                     all_pseudo_moves.append(f'{board_sqs[i]}: {rook(board_sqs[i], board)}')
-                 elif (j == BBISHOP and piece == 'B' or piece == 'b'): 
+                 elif (j == BBISHOP and piece == 'B'): 
                      all_pseudo_moves.append(f'{board_sqs[i]}: {bishop(board_sqs[i], board)}')
                  elif (j == BPAWN and piece == ''): 
                      all_pseudo_moves.append(f'{board_sqs[i]}: {pawn(board_sqs[i], board)}')
-                 elif (j == BQUEEN and piece == 'Q' or piece == 'q'):        
+                 elif (j == BQUEEN and piece == 'Q'):        
                      all_pseudo_moves.append(f'{board_sqs[i]}: {queen(board_sqs[i], board)}')
-             else:
-                 if (j == WKNIGHT and piece == 'N' or piece == 'n'):                     
+             elif (turn == WHITE and color == WHITE):
+                 if (j == WKNIGHT and piece == 'N'):                     
                      all_pseudo_moves.append(f'{board_sqs[i]}: {knight(board_sqs[i], board)}')
-                 elif (j == WKING and piece == 'K' or piece == 'k'):              
+                 elif (j == WKING and piece == 'K'):              
                      all_pseudo_moves.append(f'{board_sqs[i]}: {king(board_sqs[i], board)} ')
-                 elif (j == WROOK and piece == 'R' or piece == 'r'):
+                 elif (j == WROOK and piece == 'R'):
                      all_pseudo_moves.append(f'{board_sqs[i]}: {rook(board_sqs[i], board)} ')
-                 elif (j == WBISHOP and piece == 'B' or piece == 'b'): 
+                 elif (j == WBISHOP and piece == 'B'): 
                      all_pseudo_moves.append(f'{board_sqs[i]} {bishop(board_sqs[i], board)} ')
                  elif (j == WPAWN and piece == ''): 
                      all_pseudo_moves.append(f'{board_sqs[i]}: {pawn(board_sqs[i], board)} ')
-                 elif (j == WQUEEN and piece == 'Q' or piece == 'q'):       
+                 elif (j == WQUEEN and piece == 'Q'):       
                      all_pseudo_moves.append(f'{board_sqs[i]}: {queen(board_sqs[i], board)} ')
      
     else:
         for i,j in enumerate(board):
          if (j != EMPTY):
-             if (turn == BLACK):
+             if (turn == BLACK or color == BLACK):
                  if (j == BKNIGHT):
                      all_pseudo_moves.append(f'{board_sqs[i]}: {knight(board_sqs[i], board)}')
                  if (j == BKING):               
@@ -54,7 +54,7 @@ def generatePseudoLegalMoves(board, piece:str=None):
                      all_pseudo_moves.append(f'{board_sqs[i]}: {pawn(board_sqs[i], board)}')
                  if (j == BQUEEN):        
                      all_pseudo_moves.append(f'{board_sqs[i]}: {queen(board_sqs[i], board)}')
-             else:
+             elif (turn == WHITE or color == WHITE):
                  if (j == WKNIGHT):                     
                      all_pseudo_moves.append(f'{board_sqs[i]}: {knight(board_sqs[i], board)}')
                  if (j == WKING):              
@@ -69,4 +69,38 @@ def generatePseudoLegalMoves(board, piece:str=None):
                      all_pseudo_moves.append(f'{board_sqs[i]}: {queen(board_sqs[i], board)} ')
                      
     return all_pseudo_moves
-    
+ 
+
+def isinCheck(board, king_pos) -> bool | str:
+    color = board[king_pos][:3]
+    if (color == WHITE):
+        enmy_color = BLACK
+    elif (color == BLACK):
+        enmy_color == WHITE
+    else:
+        return 'ERR: Failed to get color'
+
+    if (king(board_sqs[king_pos], board) == []):
+        return False
+
+    black_moves = generatePseudoLegalMoves(board=board, piece=None, color=enmy_color)
+
+    for i,j in enumerate(black_moves):
+        # just getting the moves to check if anyone is seeing the other king 
+        j = j.split(':')
+        j = j[1].replace('[]','') # removing the empty moves
+        j = j.strip('[ ]') ## removing [ and ] 
+        j = j.split(',') 
+        for k in j:
+            print(k, king_pos)
+            if (k == king_pos):
+                
+                return True
+def filterlegalmoves(moves:list, board):
+    current_turn = board[64]
+    if (current_turn == WHITE):
+        king_pos = board.index(WKING)
+    elif (current_turn == BLACK):
+        king_pos = board.index(BKING)
+
+    isinCheck(board, king_pos) 
